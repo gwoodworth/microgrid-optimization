a2db57271bdb4aa7e637f517bf90d3f53f18986e
Glen Woodworth <gwoodworth@alaska.edu>
2024-12-11 23:09:27 -0900

    edits

--- functions/findc.m
+++ functions/findc.m
@@ -1,14 +1,14 @@
 function [c,p] = findc(ratings,L)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Preprocessing function to produce c and b for use with simplex. 
+%Preprocessing function to produce c for use with simplex. 
 % Only c is necessary, but p might be useful at the end to get back to the
-% actual value of the objective function.
+% actual value of the objective function (f(x)=c^Tx+p?).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
     K = length(ratings);
     n = K*L;
     [m,yInt] = genSlopeYint(ratings);
-    c = zeros(n,1);
-    p = zeros(n,1);
+    c = zeros(2*n,1); %2*n to account for the extra n slack variables
+    p = zeros(2*n,1);
     for k=1:K
         for l=1:L
             i = l + L*(k-1);
